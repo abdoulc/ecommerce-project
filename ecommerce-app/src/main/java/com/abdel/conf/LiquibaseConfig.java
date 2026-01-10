c94c3be7a5dd3a794012d42d611149f138529fbb
abdoulc <abdoulrazak1996@gmail.com>
2026-01-10 22:56:32 +0100

    (feat): Add persistence module and configure liquibase

---
+++ ecommerce-app/src/main/java/com/abdel/conf/LiquibaseConfig.java
@@ -0,0 +1,26 @@
+package com.abdel.conf;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfig {
+    private final DataSource dataSource;
+
+    @Autowired
+    public LiquibaseConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:/db/changelog/db.changelog-master.yaml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
+}
