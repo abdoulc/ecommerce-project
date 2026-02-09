cd2c99cf978f51c70b71f3eaccbd56cb4e36f630
abdoulc <abdoulrazak1996@gmail.com>
2026-02-10 00:18:37 +0100

    (chore) delete idempotency module

--- idempotency/src/main/java/com/abdel/exceptions/IdempotencyConflictException.java
+++
@@ -1,8 +0,0 @@
-package com.abdel.exceptions;
-
-public class IdempotencyConflictException extends RuntimeException {
-    public IdempotencyConflictException(String key) {
-        super("Conflict for key: " + key);
-    }
-}
-
